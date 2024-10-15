56053dde98f18ddf7edd14f6cc9d2c73f9370a18
matthias <matthias.hiemer@scitlab.de>
2024-10-15 10:01:30 +0200

    Revert "Add very dirty code"
    This reverts commit 3e9ea853bcd226d76cf526dbfbd7d4cbfeae52f2.

--- backend/src/main/java/org/example/IConstants.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.io.InputStream;
-import java.io.PrintStream;
-
-public interface IConstants {
-    public static final PrintStream OUT = System.out;
-    public static final InputStream SOURCE = System.in;
-}
